0bd63b05a6d1e157b41f4a020f993d5485ee67f5
Michael Gruber <aelgru@gmail.com>
2012-09-19 16:08:23 +0200

    using install_dependencies plugin

--- build.py
+++ build.py
@@ -1,50 +1,39 @@
 from pythonbuilder.core import use_plugin, init, Author
 
-use_plugin("python.core")
-#use_plugin("python.unittest")
-#use_plugin("python.integrationtest")
-#use_plugin("python.coverage")
-#use_plugin("python.pychecker")
-#use_plugin("python.pymetrics")
-use_plugin("python.pylint")
-use_plugin("python.distutils")
-use_plugin("python.pydev")
+use_plugin('python.core')
+use_plugin('python.install_dependencies')
+use_plugin('python.pylint')
+use_plugin('python.distutils')
+use_plugin('python.pydev')
 
-use_plugin("copy_resources")
-#use_plugin("filter_resources")
+use_plugin('copy_resources')
 
-default_task = ["analyze", "publish"]
+default_task = ['analyze', 'publish']
 
-version = "1.1.1"
-summary = "YADT - an Augmented Deployment Tool - The Broadcast Client Part"
-description = '''Yet Another Deployment Tool - The Broadcast Client Part
+version = '1.1.1'
+summary = 'YADT - an Augmented Deployment Tool - The Broadcast Client Part'
+description = """Yet Another Deployment Tool - The Broadcast Client Part
 - sends state information to a Yadt Broadcast Server instance
 - sends information on start/stop/status/update
-
-for more documentation, visit http://code.google.com/p/yadt/
-'''
-authors = [Author("Arne Hilmann", "arne.hilmann@gmail.com")]
-
-requires = "python >= 2.6 python-twisted >= 11.0.0 autobahn >= 0.4.10"
-
-url = "http://code.google.com/p/yadt"
-license = "GNU GPL v3"
+"""
+authors = [Author('Arne Hilmann', 'arne.hilmann@gmail.com')]
+url     = 'http://github.com/yadt/yadtbroadcast-client'
+license = 'GNU GPL v3'
 
 @init
 def set_properties (project):
-    project.depends_on("Twisted")
-    project.depends_on("autobahn")
+    project.depends_on('Twisted')
+    project.depends_on('autobahn')
     
-    project.set_property("coverage_break_build", False)
-    project.set_property("pychecker_break_build", False)
+    project.set_property('coverage_break_build', False)
+    project.set_property('pychecker_break_build', False)
 
-    project.get_property("distutils_commands").append("bdist_rpm")    
-    project.set_property("copy_resources_target", "$dir_dist")
-    project.get_property("copy_resources_glob").append("setup.cfg")
+    project.get_property('distutils_commands').append('bdist_rpm')    
+    project.set_property('copy_resources_target', '$dir_dist')
+    project.get_property('copy_resources_glob').append('setup.cfg')
     project.set_property('dir_dist_scripts', 'scripts')
 
-    #project.get_property("distutils_commands").append("bdist_egg")
-    project.set_property("distutils_classifiers", [
+    project.set_property('distutils_classifiers', [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
