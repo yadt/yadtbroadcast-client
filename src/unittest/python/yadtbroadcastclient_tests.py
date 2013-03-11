432d0cb80b77db3aa0af2c7399caa077c49392f4
mriehl <maximilien.riehl@gmail.com>
2013-03-11 11:19:16 +0100

    test coverage for b294cee49e579e2a1162c146cc4eaedae4c4fb43

--- src/unittest/python/yadtbroadcastclient_tests.py
+++ src/unittest/python/yadtbroadcastclient_tests.py
@@ -20,3 +20,13 @@ class YadtBroadcastClientTests(unittest.TestCase):
         WampBroadcaster.sendServiceChange(mock_broadcaster, 'data', tracking_id='tracking-id')
 
         self.assertEqual(call('service-change', 'data', 'tracking-id'), mock_broadcaster._sendEvent.call_args)
+
+
+    def test_publish_cmd_should_forward_tracking_id_to_publish_cmd_for_target(self):
+        mock_broadcaster = Mock(WampBroadcaster)
+        mock_broadcaster.target = 'target'
+
+        WampBroadcaster.publish_cmd(mock_broadcaster, 'command', 'state', message='message', tracking_id='tracking-id')
+
+        actual_call = mock_broadcaster.publish_cmd_for_target.call_args
+        self.assertEqual(call('target', 'command', 'state', 'message', 'tracking-id'), actual_call)
