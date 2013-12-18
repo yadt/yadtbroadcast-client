95f22e3ed4946185ff19fce2f46ecc098be055ff
mriehl <maximilien.riehl@gmail.com>
2013-12-18 13:56:13 +0100

    we don't use pydev

--- build.py
+++ build.py
@@ -3,7 +3,6 @@ from pybuilder.core import use_plugin, init, Author
 use_plugin('python.core')
 use_plugin('python.install_dependencies')
 use_plugin('python.distutils')
-use_plugin('python.pydev')
 use_plugin('python.flake8')
 
 use_plugin('python.unittest')
